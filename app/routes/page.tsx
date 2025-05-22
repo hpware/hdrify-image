6c4abc1cb88ddaf9cafa0aa04c8e0b17bfba6371
吳元皓 <hw@yuanhau.com>
2025-05-22 11:38:08 +0800

    Made a basic home page.

--- app/routes/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function page() {
-  return (
-    <div className="flex flex-col justify-center align-center text-center">
-      <h1 className="text-8xl">Hi</h1>
-    </div>
-  );
-}
